235430d86ea75ccc61edc6e5f5bab3e2ecfb3b6e
Prince Akrong <akrongprince2005@gmail.com>
2024-07-28 20:13:43 -0700

    execute terraform plan

---
+++ src/Iam.tf
@@ -0,0 +1,34 @@
+module "eks_admins_iam_role" {
+  source = "terraform-aws-modules/iam//modules/iam-assumable-role"
+  version = "5.3.1"
+
+  role_name         = "eks-admin"
+  create_role       = true
+  role_requires_mfa = false
+
+  custom_role_policy_arns = [module.allow_eks_access_iam_policy.arn]
+
+  trusted_role_arn = [
+    "arn:aws:iam::${module.vpc.vpc_owner_id}:root"
+  ]
+}
+
+data "aws_iam_policy_document" "eks_access" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "eks:DescribeCluster"
+    ]
+    resources = [
+      "arn:aws:eks:${var.aws_region}:${data.aws_caller_identity.current.account_id}:cluster/${local.cluster_name}"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "eks_access" {
+  name   = "eks-access"
+  role   = module.eks_admins_iam_role.role_name
+  policy = data.aws_iam_policy_document.eks_access.json
+}
+
+data "aws_caller_identity" "current" {}
