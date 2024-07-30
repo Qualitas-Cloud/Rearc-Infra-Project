4c512671e48f7f04d8081fbc584e4e5c4e04feeb
Prince Akrong <akrongprince2005@gmail.com>
2024-07-30 04:58:08 -0700

    execute terraform plan(skip ci)

--- src/eks-iam-role/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "eks_admin_role_arn" {
-  value       = module.eks_admins_iam_role.this_iam_role_arn
-  description = "IAM role ARN for EKS admin access"
-}
--- src/outputs.tf
+++ src/outputs.tf
@@ -21,9 +21,8 @@ output "region" {
 output "oidc_provider_arn" {
   value = module.eks.oidc_provider_arn
 }
-
 output "eks_admin_role_arn" {
-  value       = module.eks_admins_iam_role.role.arn
+  value       = module.eks_admins_iam_role.role_arn
   description = "IAM role ARN for EKS admin access"
 }
 
